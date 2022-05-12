2c748a12f59f004bec943e93e0ea429e0576afe4
fastcode <fastcode@outlook.es>
2022-05-12 22:11:42 +0200

    Change literal and create modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,11 @@ import About from "../components/About/About";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 import FixSocialIcon from "../components/SocialIcon/FixSocialIcon";
+import ServerModal from "../components/Modal/Modal";
 function Home() {
   return (
     <>
+      <ServerModal />
       <Hero />
       <Projects />
       <About />
